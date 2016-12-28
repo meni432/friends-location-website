6087ff9165c37abb9b4574646cdf41f76f8726ab
Meni Samet <meni432@gmail.com>
2016-12-28 23:20:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items: FirebaseListObservable<any[]>;
+  latitude : number;
+  longitude : number;
+
+  constructor(public af: AngularFire){
+    this.af.auth.subscribe(auth => this.onAuth(auth));
+    this.items = af.database.list('/hex_to_key');
+  }
   title = 'app works!';
+
+  login() {
+    this.af.auth.login();
+  }
+
+  logout() {
+     this.af.auth.logout();
+  }
+
+
+  loadMap() {
+    console.log('load map: lat: ' + this.latitude + ' lot: ' + this.longitude);
+  }
+
+
+  // add data listener to database current last Location
+  onAuth(auth : any) {
+    var path : string = '/user_detail/'+auth.uid+'/lastLocation/';
+    // this.items = this.af.database.list(path);
+    this.af.database.list(path, { preserveSnapshot: true})
+    .subscribe(snapshots=>{
+        snapshots.forEach(snapshot => {
+          console.log(snapshot.key, snapshot.val());
+          if (snapshot.key == 'latitude') {
+            console.log('latitude: ' + snapshot.val());
+            this.latitude = snapshot.val();
+          }
+          if (snapshot.key == 'longitude') {
+            console.log('longitude: ' + snapshot.val());
+            this.longitude = snapshot.val();
+          }
+        });
+
+        this.loadMap();
+
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCi1Ll3jl5nxtJRuHTklcB4A12dpd2O50A',
+  authDomain: 'friends-location-50a95.firebaseapp.com',
+  databaseURL: 'https://friends-location-50a95.firebaseio.com',
+  storageBucket: 'friends-location-50a95.appspot.com',
+  messagingSenderId: '355395218260'
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+}
+
 
 @NgModule({
   declarations: [
@@ -12,7 +29,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCmxeT2DGRVTb-ZlJqGcWLY7A_5vJymWG0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
